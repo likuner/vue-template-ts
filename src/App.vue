0c35ded6f23895bf16af7b0b6c34730eda37f137
likun <273676636@qq.com>
2023-08-04 13:13:59 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import './components/HoverTipWebComponent/index.ts';
+import './components/HoverTipWebComponent/index.js';
 
 export default defineComponent({
     data() {
