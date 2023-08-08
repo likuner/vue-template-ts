1f80b150992f7d07afd5b93da66b37c1bf15cef7
likun <273676636@qq.com>
2023-08-08 17:35:38 +0800

    update

--- src/components/HoverTipWebComponent/raw.js
+++ src/components/HoverTipWebComponent/raw.js
@@ -1,3 +1,5 @@
+import styles from './index.css';
+
 class MyHoverTip extends HTMLElement {
     constructor() {
         super();
@@ -57,65 +59,7 @@ class MyHoverTip extends HTMLElement {
         }
 
         const sheet = new CSSStyleSheet();
-        sheet.replaceSync(`
-            .hover-tip {
-                width: 100vw;
-                height: 100vh;
-                box-sizing: border-box;
-                overflow: hidden;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                align-items: center;
-                position: fixed;
-                top: 0;
-                left: 0;
-                z-index: 99999;
-                background: rgba(0, 0, 0, 0.7);
-                padding: 20vh 0 30vh;
-            }
-            .title-box {
-                height: 1rem;
-                width: 86.6vw;
-                max-width: 750px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-size: 0.36rem;
-                font-weight: bold;
-                color: #363636;
-                background: #ffffff;
-                border-radius: 0.3rem;
-                position: relative;
-            }
-            .title-box::after {
-                content: '';
-                width: 0;
-                height: 0;
-                border-top: 0.3rem solid #ffffff;
-                border-right: 0.3rem solid transparent;
-                border-left: 0.3rem solid transparent;
-                position: absolute;
-                z-index: 9;
-                bottom: -0.2rem;
-                left: 50%;
-                transform: translateX(-50%);
-            }
-            .btn-box {
-                height: 1.2rem;
-                width: 48.8vw;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-size: 0.4rem;
-                font-weight: bold;
-                color: #ffffff;
-                padding-bottom: 1px;
-                background: no-repeat url('https://qiniu.whjiaoy.com/xbyschool/m6BcwUv7xp9aDNyMPHECC9L7rWmeIc3q') center
-                    center;
-                background-size: contain;
-            }
-        `);
+        sheet.replaceSync(styles);
         this.shadowRoot.adoptedStyleSheets = [sheet];
     }
 }
